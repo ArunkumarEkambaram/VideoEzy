8c440fc35a7c0b10a610e50ba3add1fac267d4bb
Arunkumar Ekambaram <earun2mail@gmail.com>
2019-01-03 08:50:48 +0530

    Added New Solution - VideoEzy using MVC

---
+++ VideoEzy_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoEzy_MVC.MvcApplication" Language="C#" %>
---
+++ VideoEzy_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoEzy_MVC.Startup))]
+namespace VideoEzy_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
