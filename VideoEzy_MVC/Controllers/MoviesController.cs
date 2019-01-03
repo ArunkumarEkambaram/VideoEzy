982aa7b11105b8159e04f713aac5fc85a156be83
Arunkumar Ekambaram <earun2mail@gmail.com>
2019-01-03 18:47:09 +0530

    Enable Code First Migration - added Identity Tables

--- VideoEzy_MVC/Controllers/MoviesController.cs
+++ VideoEzy_MVC/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using VideoEzy_MVC.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace VideoEzy_MVC.Controllers
 {
@@ -8,9 +10,26 @@ namespace VideoEzy_MVC.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            return View();
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().FirstOrDefault(m => m.Id == id);
+            return View(movie);
         }
         
-        
+        public List<Movie> GetMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie{Id=1,Name="Soodhukavum"},
+                new Movie{Id=2,Name="Junga"},
+                new Movie{Id=3,Name="96"}
+            };
+
+            return movies;
+        }
     }
 }
